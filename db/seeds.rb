26daa6a934a4e39e5bce48f34af65f3cdbdba1c2
nndd91 <nndd91@hotmail.com>
2017-06-07 22:44:52 +0800

    Add column to user and also seed and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create first admin user
+email = "demo@demo.com"
+password = "password"
+first_name = "Demo"
+last_name = Faker::Name.last_name
+username = "Demo"
+is_admin = true
+
+User.create(username: username, email: email, password: password, password_confirmation: password,
+            first_name: first_name, last_name: last_name)
+
+# Creating Remaining Users
+20.times do
+  username = Faker::Internet.user_name
+  email = Faker::Internet.email
+  password = "password"
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(username: username, email: email, password: password, password_confirmation: password,
+              first_name: first_name, last_name: last_name)
+end
