60c8c404f61406b19fd8b25120da5992226423b0
nndd91 <nndd91@hotmail.com>
2017-06-07 23:56:51 +0800

    Generate form controller and update view

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,26 @@
+class FormsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def index
+    @forms = Form.all
+  end
+
+  def attempt
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :views
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :forms do
+    member do
+      get 'attempt'
+    end
+  end
 end
