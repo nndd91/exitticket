bbe4b43badfc6006cec18d576e0bf8da3ea2acf8
nndd91 <nndd91@hotmail.com>
2017-06-08 00:50:31 +0800

    Add first attempt form

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -17,11 +17,31 @@ class FormsController < ApplicationController
   def edit
   end
 
+
+
   def index
     @forms = Form.all
   end
 
   def attempt
     @form = Form.find(params[:id])
+  end
+
+  def attempting
+    @form = Form.find(params[:id])
+
+    array = [:q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10]
+    @form.questions.each_with_index do |question, index|
+      @answer = question.answers.build(content: params[:form][array[index]], form: @form)
+      @answer.user = current_user
+      @answer.save
+    end
+
+    redirect_to forms_path
+  end
+
+  private
+
+  def attempting_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :forms do
     member do
       get 'attempt'
+      post 'attempting'
     end
   end
 end
