e68dc025cc4ac93a19627575313ecf46f964b50d
nndd91 <nndd91@hotmail.com>
2017-06-08 16:40:31 +0800

    Generate form_template controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'formtemplates/index'
+
+  get 'formtemplates/show'
+
+  get 'formtemplates/new'
+
+  get 'formtemplates/create'
+
+  get 'formtemplates/edit'
+
+  get 'formtemplates/update'
+
+  get 'formtemplates/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
