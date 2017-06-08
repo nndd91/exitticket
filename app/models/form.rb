7c83f0ffa550d1e7d2f2887b6eda32bb2db7e895
nndd91 <nndd91@hotmail.com>
2017-06-08 18:55:23 +0800

    Fix bug with questions list and also form questions list

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,7 +2,7 @@ class Form < ApplicationRecord
   belongs_to :form_template
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :questions, through: :formtemplates
+  has_many :questions, through: :form_template
   has_many :logs, dependent: :destroy
 
   attr_accessor :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10
