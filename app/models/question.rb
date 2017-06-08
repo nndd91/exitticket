da9f2eb1c54045b17907e54ba76742aa8151faad
nndd91 <nndd91@hotmail.com>
2017-06-08 23:55:56 +0800

    Add enum to collection and set default template

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :form_template
   has_many :answers, dependent: :destroy
+
+  enum qns_type: { "Text Field": 1, "1 to 5 Collection": 2, "Text Area":3 }
 end
